126fc399e72941a45795e149072870bae897e0d5
Victor Veytsman <vityavv@gmail.com>
2018-01-18 14:38:39 -0500

    Did some comments

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
+//VARS
 let members = [];
 let money = 700;
-
-function removeMember(button) {
+let cycle = 0;
+//Concerning the family
+function removeMember(button) {//Remove a member of the family
 	button.parentNode.remove();
 }
-function addMember() {
+function addMember() {//add a member of the family
 	let element = document.createElement("div");
-	element.setAttribute("class", "member");
+	element.setAttribute("class", "member");//create a new element, make it a member, and put the normal stuff inside it
 	element.innerHTML = "Name: <input/> <button onClick='removeMember(this)'>Remove</button>";
-	document.getElementById("family").appendChild(element);
+	document.getElementById("family").appendChild(element);//add it to the DOM
 }
-function startGame() {
-	let memberElements = Array.from(document.querySelectorAll(".member > input"));
-	if (memberElements.length <= 0) {
+function startGame() {//Start the game
+	let memberElements = Array.from(document.querySelectorAll(".member > input"));//get all of the member input elements
+	if (memberElements.length <= 0) {//alert if there aren't any
 		alert("You can't have a family with no members!");
-		return;
+		return;//exit function
 	}
 	memberElements.forEach(memberElement => {
-		members.push(new Person(memberElement.value));
+		members.push(new Person(memberElement.value));//for each element, make a new person with their name
 	});
-	console.log(members);
 }
 
-function Person(name) {
+function Person(name) {//Constructor for making a new person
 	this.job = 0;
 	this.alcoholic = false;
 	this.name = name;
