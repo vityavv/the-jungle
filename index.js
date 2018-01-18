8133491dfad989a3830ed161cfe992e3eef2187f
Victor Veytsman <vityavv@gmail.com>
2018-01-18 14:36:07 -0500

    Add a dwelling menu and family-making function

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-function removeMember(thing) {
-	thing.parentNode.remove();
+let members = [];
+let money = 700;
+
+function removeMember(button) {
+	button.parentNode.remove();
 }
 function addMember() {
 	let element = document.createElement("div");
 	element.setAttribute("class", "member");
 	element.innerHTML = "Name: <input/> <button onClick='removeMember(this)'>Remove</button>";
 	document.getElementById("family").appendChild(element);
+}
+function startGame() {
+	let memberElements = Array.from(document.querySelectorAll(".member > input"));
+	if (memberElements.length <= 0) {
+		alert("You can't have a family with no members!");
+		return;
+	}
+	memberElements.forEach(memberElement => {
+		members.push(new Person(memberElement.value));
+	});
+	console.log(members);
+}
+
+function Person(name) {
+	this.job = 0;
+	this.alcoholic = false;
+	this.name = name;
 }
