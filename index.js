72b80b2c2e806a45485e15d534d6895eebecadfa
Victor Veytsman <vityavv@gmail.com>
2018-01-19 10:55:56 -0500

    Do some commenting and fix a tiny bug

--- index.js
+++ index.js
@@ -39,59 +39,60 @@ function Person(name) {//Constructor for making a new person
 //Choosing a house
 function chooseDwelling(chosenDwelling) {
 	if (chosenDwelling === "house") {
-		if (money >= 300) {
+		if (money >= 300) {//make sure you have enough money
 			dwelling = "house";
-			house = 10;
+			house = 10;//this is a counting-down number---when it reaches 0 you stop paying
 			money -= 300;
 		} else {
-			alert("You don't have enough money for that!");
+			alert("You don't have enough money for that!");//if you don't...
 			return;
 		}
 	} else if (chosenDwelling === "rent") {
 		dwelling = "rent";
-		house = 0;
+		house = 0;//reset the counting-down if someone loses their house
 	} else {
 		dwelling = "homeless";
-		house = 0;
+		house = 0;//^^^
 	}
 	document.getElementById("job").style.display = "block";
-	document.getElementById("dwelling").style.display = "none";
+	document.getElementById("dwelling").style.display = "none";//go to the next screen
 	document.getElementById("jobChooser").innerText = members[jobChooser].name;
 }
 function chooseJob(element, skip) {
-	if (skip) return;
+	if (skip) return;//if you're not getting a job or already have one, skip!
 
 	Array.from(document.getElementsByClassName("job")).forEach(el => {el.style.visibility = "hidden"});
-	element.style.visibility = "visible";
+	element.style.visibility = "visible";//make sure you can only see the one you picked
 	element.style.background = "white";
 
-	let job = [0, 3, 5][Math.floor(Math.random()*3)];
-	if (job === 0) element.innerHTML = "<h1>You didn't get a job :(</h1><br>";
+	let job = [0, 3, 5][Math.floor(Math.random()*3)];//see what job you get---completely random
+	if (job === 0) element.innerHTML = "<h1>You didn't get a job :(</h1><br>";//Here and below; show what job you got and your options
 	else element.innerHTML = `<h1>You got a job that pays $${job}!</h1><br><span id="bribe"><button onClick="bribe()">Bribe the boss for double pay - $20</button></span><br><br>`;
 	element.innerHTML += "<button onClick='nextChoose(this)'>Next</button>";
-	element.removeAttribute("onClick");
+	element.removeAttribute("onClick");//make sure that it doesn't change when you click on it by taking that away
 
-	members[jobChooser].job = job;
+	members[jobChooser].job = job;//sets the job for real now
 }
 function bribe() {
 	if (money < 20) {
-		alert("You don't have enough money!");
+		alert("You don't have enough money!");//yelling!
 		return;
 	}
-	members[jobChooser].job *= 2;
-	document.getElementById("bribe").innerHTML = `Your job now earns you $${members[jobChooser].job}!`;
+	money -= 20;//take away money
+	members[jobChooser].job *= 2;//double pay
+	document.getElementById("bribe").innerHTML = `Your job now earns you $${members[jobChooser].job}!`;//tell 'em
 }
 function nextChoose(element) {
-	jobChooser++;
+	jobChooser++;//next jobChooser
 	if (!members[jobChooser]) {
 		//TODO: go to game
 		return;
 	}
-	document.getElementById("jobChooser").innerText = members[jobChooser].name;
-	Array.from(document.getElementsByClassName("job")).forEach(el => {el.style.visibility = "visible"});
+	document.getElementById("jobChooser").innerText = members[jobChooser].name;//show their name
+	Array.from(document.getElementsByClassName("job")).forEach(el => {el.style.visibility = "visible"});//make sure you can see all of the options
 	let parent = element.parentNode;
-	parent.style.background = "lightcyan";
+	parent.style.background = "lightcyan";//make them light-cyan again
 	setTimeout(()=>{parent.setAttribute("onClick", "chooseJob(this)")}, 100);//some things I don't like, and this is one of them
-	parent.innerHTML = "";
+	parent.innerHTML = "";//take away the text
 }
 //Game functions:
