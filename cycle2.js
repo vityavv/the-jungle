046c6abff1966983e2da1dbb3b18925f609c2942
Victor Veytsman <vityavv@gmail.com>
2018-02-05 10:30:54 -0500

    there's a con artist in town, and he's comin' for your money!

--- cycle2.js
+++ cycle2.js
@@ -1,5 +1,5 @@
 function chooseCycleTwo(element) {
-	let chosen = Math.floor(Math.random()*6);chosen = 0;
+	let chosen = Math.floor(Math.random()*6);chosen = 1;
 	let member, string;
 	let jobMembers = members.filter(person => person.job !== 0);
 	switch (chosen) {
@@ -14,6 +14,13 @@ function chooseCycleTwo(element) {
 			string = `"${member.name}" went out into the cold and froze up in a blizzard, so they missed a day at work. They will lose their job unless you can convince the boss to let them back.`;
 			updateStatus(string);
 			element.innerHTML = `<h3>${string}</h3><br><br><span><button onClick='bribeToKeepJobHard(this)'>Convince the boss to keep the job - $30</button><br><br><button onClick='frozen(this, ${members.indexOf(member)})'>Quit your job</button></span>`;
+			break;
+		case 1:
+			let stolen = (Math.floor(Math.random()*6)+1)*10;
+			money -= stolen;
+			string = `Your family met a con artist and got conned for $${stolen}`;
+			updateStatus(string);
+			element.innerHTML = `<h3>${string}</h3><br><br><button onClick='nextCycleChoose(this)'>Next</button>`;
 			break;
 	}
 }
